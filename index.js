36509467d7f433af3bf454796026bbf5e2125f13
minhajmia <minhajmia418@gmail.com>
2022-11-08 10:22:22 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
@@ -9,8 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD_NAME}@cluster0.dftbcru.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("mongo connect");
+  client.close();
+});
+
 app.get("/", (req, res) => {
-  res.send("server is running for assignment-11");
+  res.send("server is running for service Review");
 });
 app.listen(port, () => {
   console.log("listening the port of  : ", port);
